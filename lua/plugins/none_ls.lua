2f37feeda9d418473d99fccdc19ec6a11864c17a
Sweetoos <thespbsik@gmail.com>
2025-09-16 13:29:43 +0200

    fix: python lsp

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -1,21 +1,44 @@
 return {
-    "nvimtools/none-ls.nvim",
-    lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        local null_ls = require("null-ls")
+    {
+        "nvimtools/none-ls.nvim",
+        lazy = false,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                -- FORMATTERS
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.black, -- for Python
-                null_ls.builtins.formatting.isort, -- for Python
-                null_ls.builtins.formatting.clang_format.with({
-                    filetypes = { "c", "cpp", "objc", "objcpp" },
-                }),
-            },
-        })
-    end,
+            null_ls.setup({
+                sources = {
+                    -- FORMATTERS
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black, -- for Python
+                    null_ls.builtins.formatting.isort, -- for Python
+                    null_ls.builtins.formatting.clang_format.with({
+                        filetypes = { "c", "cpp", "objc", "objcpp" },
+                    }),
+                },
+            })
+        end,
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        lazy = false,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function()
+            require("mason-null-ls").setup({
+                ensure_installed = {
+                    "black",
+                    "isort",
+                    "prettier",
+                    "stylua",
+                    "clang-format",
+                },
+                automatic_installation = true,
+                handlers = {},
+            })
+        end,
+    },
 }
